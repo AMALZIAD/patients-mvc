e9db657bd05527544af6b71f56574baa9aa355f1
amal ziad <git config --global user.email>
2022-09-20 16:07:28 +0300

    jdbcAuthentication

--- src/main/java/ma/enset/patientsmvc/repositories/MedecinRepository.java
+++ src/main/java/ma/enset/patientsmvc/repositories/MedecinRepository.java
@@ -1,7 +1,7 @@
 package ma.enset.patientsmvc.repositories;
 
 import ma.enset.patientsmvc.entities.Medecin;
-import ma.enset.patientsmvc.entities.Patient;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
